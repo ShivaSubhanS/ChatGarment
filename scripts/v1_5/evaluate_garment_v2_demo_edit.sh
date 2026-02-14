e7696911bf12da38957b2d42f8d77d00f8673194
ShivaSubhanS <shivagamings2005@gmail.com>
2026-02-14 14:47:34 +0530

    bug fix

--- scripts/v1_5/evaluate_garment_v2_demo_edit.sh
+++ scripts/v1_5/evaluate_garment_v2_demo_edit.sh
@@ -40,7 +40,7 @@ deepspeed scripts/evaluate_garment_v2_demo_edit_1float.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 3072 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
--- scripts/v1_5/finetune_task_lora_garmentcode_outfit.sh
+++ scripts/v1_5/finetune_task_lora_garmentcode_outfit.sh
@@ -40,7 +40,7 @@ deepspeed llava/train/train_mem_garmentcode_outfit.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 3072 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
